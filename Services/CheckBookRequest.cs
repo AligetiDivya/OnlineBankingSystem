cb766988c27f06c2f0c46c374cb5bd9a56be6791
Divya Aligeti <aligeti.divya1997@gmail.com>
2025-04-30 21:38:01 -0500

    Reapply "Add project files."
    This reverts commit 24ad2846b213d0ec489061b047dcce8a62bf80a9.

---
+++ Services/CheckBookRequest.cs
@@ -0,0 +1,13 @@
+﻿using OnlineBankingSystem.Models;
+using System;
+
+namespace OnlineBankingSystem.Services
+{
+    public class CheckBookRequest
+    {
+        public static void Request(CustomerAccount account)
+        {
+            Console.WriteLine($"Checkbook requested for account {account.AccountNumber}");
+        }
+    }
+}
