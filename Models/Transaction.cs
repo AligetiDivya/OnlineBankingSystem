44017c5670bde540edfa98d756a8fdc2b8a99e13
Divya Aligeti <aligeti.divya1997@gmail.com>
2025-05-19 17:34:48 -0500

    made it simple

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,18 +1,7 @@
-﻿namespace OnlineBankingSystem.Models
+public class Transaction
 {
-    public class Transaction
-    {
-        public DateTime TransactionDate { get; set; }
-        public string Description { get; set; }
-        public double Amount { get; set; }
-        public string TransactionType { get; set; }
-
-        public Transaction(DateTime transactionDate, string description, double amount, string transactionType)
-        {
-            TransactionDate = transactionDate;
-            Description = description;
-            Amount = amount;
-            TransactionType = transactionType;
-        }
-    }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }       
+    public decimal Amount { get; set; }
+    public string Description { get; set; }
 }
